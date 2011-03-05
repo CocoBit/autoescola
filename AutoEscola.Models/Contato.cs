9f6551e7687865f982717d03c53d37551568e7f8
Bruno Borges <bbgbruno@gmail.com>
2011-03-05 20:13:53 -0300

    Incluido o modelo de Contato

---
+++ AutoEscola.Models/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoEscola.Interfaces.Models;
+
+
+namespace AutoEscola.Models
+{
+    public class Contato : IModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Twitter { get; set; }
+        public string Fax { get; set; }
+
+    }
+}
