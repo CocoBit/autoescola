c7ffa4a5a161d7895f20c9a1fb7bd8c5cc300f09
yan <yanlimaj@gmail.com>
2011-08-28 07:03:18 -0300

    Atualização do projeto e adição da classe contato

---
+++ AutoEscola/Helpers/Contato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+
+namespace AutoEscola.Helpers
+{
+    public class Contato: Controller
+    {
+        [HttpPost]
+        public ActionResult Enviar(string nome, string email, string assunto, string mensagem)
+        {
+            WebMail.SmtpServer = "smtp.gmail.com";
+            WebMail.From = "yanlimaj@gmail.com";
+            WebMail.UserName = "yanlimaj@gmail.com";
+            WebMail.Password = "ty210107myx5004";
+            WebMail.Send(email, assunto, mensagem);
+            return View();
+        }
+    }
+
+
+
+}
